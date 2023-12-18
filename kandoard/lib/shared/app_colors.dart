2df7201529d4fda370f845e842349c656017c7eb
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-12-17 21:58:44 -0300

    feat: classe cores de uso geral no app

---
+++ kandoard/lib/shared/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color grey = const Color.fromARGB(255, 52, 52, 52);
+  static Color blue = const Color(0xFF7398C8);
+  static Color white = const Color(0xFFD9D9D9);
+}

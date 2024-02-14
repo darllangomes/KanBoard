9c188623c5558c95709361e0a59a0fedae00d17d
carlosgabri311 <carlosgabriel123456@gmail.com>
2024-02-14 17:23:34 -0300

    Criação das funções de verificação de email e senha

--- kandoard/test/widget_test.dart
+++ kandoard/test/widget_test.dart
@@ -1,13 +1,7 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:kandoard/controller/login_controller.dart' as login_controller;
+import 'package:kandoard/controller/register_controller.dart' as register_controller;
 import 'package:kandoard/main.dart';
 
 void main() {
@@ -30,15 +24,15 @@ void main() {
     expect(find.text('Login no KanBoard'), findsOneWidget);
   });
   test('Validação de Email no Login', () {
-    expect(validarEmail('usuario@example.com'), true);
-    expect(validarEmail('email_invalido'), false);
+    expect(login_controller.validarEmail('usuario@example.com'), true);
+    expect(login_controller.validarEmail('email_invalido'), false);
   });
   test('Validação de Senha no Login', () {
-  expect(validarSenha('senha123'), true);
-  expect(validarSenha('abc'), false);
-});
-test('Confirmação de Senha no Cadastro', () {
-  expect(confirmarSenha('senha123', 'senha123'), true);
-  expect(confirmarSenha('senha123', 'outrasenha'), false);
-});
+    expect(login_controller.validarSenha('senha123'), true);
+    expect(login_controller.validarSenha('abc'), false);
+  });
+  test('Confirmação de Senha no Cadastro', () {
+    expect(register_controller.confirmarSenha('senha123', 'senha123'), true);
+    expect(register_controller.confirmarSenha('senha123', 'outrasenha'), false);
+  });
 }

0dc76785eb3c91d11ba6c19262a9b7b59062c9ed
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-16 16:23:47 -0300

    :sparkles: feat: adicionar validacao para os campos do textformfield

--- kandoard/lib/components/custom_text_field.dart
+++ kandoard/lib/components/custom_text_field.dart
@@ -9,22 +9,38 @@ class CustomTextField extends StatelessWidget {
   TextInputType keyboardInputType;
   final bool isObscureText;
   final TextEditingController controller;
+  String errorLabel;
 
   CustomTextField(
       {super.key,
       required this.label,
       required this.labelIcon,
       required this.controller,
+      required this.errorLabel,
       this.keyboardInputType = TextInputType.text,
       this.isObscureText = false});
 
   @override
   Widget build(BuildContext context) {
+    TextFieldController inputController = TextFieldController();
     return Padding(
       padding: const EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 0),
       child: TextFormField(
-        onChanged: (text) {
+        controller: controller,
+        validator: (value) {
+          //TODO: refatorar utilização dos if's encadeados para outra coisa
+          //TODO: validação de confirmação de senha do usuario - para saber se senhas são iguais
+          if (value == null || value.isEmpty) {
+            return errorLabel;
+          } else if (keyboardInputType == TextInputType.emailAddress) {
+            return inputController.validationEmail(value);
+          } else if (keyboardInputType == TextInputType.text) {
+            return inputController.validationPassword(value);
+          } else {
+            return null;
+          }
         },
+        onChanged: (text) {},
         obscureText: isObscureText,
         keyboardType: keyboardInputType,
         style: TextStyle(color: AppColors.white, fontWeight: FontWeight.w300),
--- kandoard/lib/controller/textfield_controller.dart
+++ kandoard/lib/controller/textfield_controller.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class TextFieldController extends ChangeNotifier {
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController passwordConfirmController =
+      TextEditingController();
   final TextEditingController genericTextController = TextEditingController();
   String _errorInput = '';
   String get errorInput => _errorInput;
@@ -16,14 +17,38 @@ class TextFieldController extends ChangeNotifier {
     return passwordController.text;
   }
 
+  String getPasswordConfirmFromTextField() {
+    return passwordController.text;
+  }
+
   String getGenericTextFromTextField() {
     return genericTextController.text;
   }
+
+  String? validationEmail(String value) {
+    if (!value.contains(RegExp(
+        r'^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$'))) {
+      return 'Digite um email valido';
+    } else {
+      return null;
+    }
+  }
+
+  String? validationPassword(String value) {
+    if (value.length < 8) {
+      return 'A senha precisa ter no minimo 8 caracteres';
+    }
+    return null;
+  }
+
+  String? test(value, errorLabel) {
+    if (value == null || value.isEmpty) {
+      return errorLabel;}
+      return null;
+    }
 
   void setErrorMenssage(String messageError) {
     _errorInput = messageError;
     notifyListeners();
   }
-
-
 }

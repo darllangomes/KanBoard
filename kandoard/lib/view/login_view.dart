31e9ffd92941dd58a839153ef31d16ff2c8aed59
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-16 16:55:07 -0300

    :sparkles: feat: modificar fomulario login para Form()

--- kandoard/lib/view/login_view.dart
+++ kandoard/lib/view/login_view.dart
@@ -14,129 +14,137 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     TextFieldController textFieldController = TextFieldController();
+    final _LoginformKey = GlobalKey<FormState>();
     return Scaffold(
       backgroundColor: AppColors.grey,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 26.0, right: 26),
-            child: Column(
-              children: [
-                const Text.rich(TextSpan(children: [
-                  TextSpan(
-                      text: 'Login no ',
-                      style: TextStyle(
-                          color: Color(0xFF7398C8),
-                          fontWeight: FontWeight.w300,
-                          fontSize: 24)),
-                  TextSpan(
-                      text: 'KanBoard',
-                      style: TextStyle(
-                          color: Color(0xFF7398C8),
-                          fontWeight: FontWeight.w600,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 24))
-                ])),
-                const SizedBox(
-                  height: 25,
-                ),
-                CustomTextField(
-                  label: 'Email',
-                  labelIcon: Icons.email,
-                  keyboardInputType: TextInputType.emailAddress,
-                  controller: textFieldController.emailController,
-                  errorLabel: 'Digite seu email',
-                ),
-                const SizedBox(height: 26,),
-                CustomTextField(
-                  label: 'Senha',
-                  labelIcon: Icons.password,
-                  isObscureText: true,
-                  controller: textFieldController.passwordController,
-                  errorLabel: 'Digite sua senha',
-                ),
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/projetos');
-                    },
-                    child: Stack(
-                      children: [
-                        const Text(
-                          'Esqueci minha senha',
-                          style: TextStyle(
+      body: Form(
+        key: _LoginformKey,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 26.0, right: 26),
+              child: Column(
+                children: [
+                  const Text.rich(TextSpan(children: [
+                    TextSpan(
+                        text: 'Login no ',
+                        style: TextStyle(
                             color: Color(0xFF7398C8),
                             fontWeight: FontWeight.w300,
-                            fontSize: 16,
+                            fontSize: 24)),
+                    TextSpan(
+                        text: 'KanBoard',
+                        style: TextStyle(
+                            color: Color(0xFF7398C8),
+                            fontWeight: FontWeight.w600,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 24))
+                  ])),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  CustomTextField(
+                    label: 'Email',
+                    labelIcon: Icons.email,
+                    keyboardInputType: TextInputType.emailAddress,
+                    controller: textFieldController.emailController,
+                    errorLabel: 'Digite seu email',
+                  ),
+                  const SizedBox(height: 26,),
+                  CustomTextField(
+                    label: 'Senha',
+                    labelIcon: Icons.password,
+                    isObscureText: true,
+                    controller: textFieldController.passwordController,
+                    errorLabel: 'Digite sua senha',
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/projetos');
+                      },
+                      child: Stack(
+                        children: [
+                          const Text(
+                            'Esqueci minha senha',
+                            style: TextStyle(
+                              color: Color(0xFF7398C8),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 16,
+                            ),
                           ),
-                        ),
-                        Positioned(
-                          bottom: 2,
-                          left: 8,
-                          right: 2,
-                          child: Container(
-                            height: 1,
-                            color: const Color(0xFF7398C8),
-                          ),
-                        )
-                      ],
+                          Positioned(
+                            bottom: 2,
+                            left: 8,
+                            right: 2,
+                            child: Container(
+                              height: 1,
+                              color: const Color(0xFF7398C8),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 56,
-                  width: 196,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.blue,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () {},
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: AppColors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w300),
-                      )),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  'Não possui uma conta?',
-                  style: TextStyle(
-                      color: Color(0XFFD9D9D9),
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                SizedBox(
-                  height: 56,
-                  width: 196,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.grey,
-                          side: BorderSide(color: AppColors.blue),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () =>
-                          Navigator.popAndPushNamed(context, '/register'),
-                      child: Text(
-                        "Cadastrar",
-                        style: TextStyle(
-                            color: AppColors.blue,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w300),
-                      )),
-                ),
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 56,
+                    width: 196,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onPressed: () {
+                          if(_LoginformKey.currentState!.validate()) {
+                            print('login');
+                          }
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: AppColors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w300),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    'Não possui uma conta?',
+                    style: TextStyle(
+                        color: Color(0XFFD9D9D9),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    height: 56,
+                    width: 196,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.grey,
+                            side: BorderSide(color: AppColors.blue),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onPressed: () =>
+                            Navigator.popAndPushNamed(context, '/register'),
+                        child: Text(
+                          "Cadastrar",
+                          style: TextStyle(
+                              color: AppColors.blue,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w300),
+                        )),
+                  ),
+                ],
+              ),
             ),
           ),
         ),

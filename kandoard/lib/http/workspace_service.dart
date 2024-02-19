ff263902cfaea60ead50d51f21a95e8f8505e91e
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-18 22:01:20 -0300

    :sparkle: criar WorkspaceService

---
+++ kandoard/lib/http/workspace_service.dart
@@ -0,0 +1,53 @@
+import 'package:dio/dio.dart';
+import '../model/user_model.dart';
+import '../model/workspace_model.dart';
+
+
+class WorkspaceService {
+  final dio = Dio();
+
+  Future<List<WorkspaceModel>> workspaceGetAll() async {
+    dio.options.headers['Authorization'] =
+        "Bearer ${UserLogged.first.getUserId}";
+
+    try {
+      final response =
+          await dio.get('https://kanbanboard-nj8m.onrender.com/api/workspace');
+
+      List<WorkspaceModel> workspace = [];
+
+      response.data.map((item) {
+       final w = WorkspaceModel.fromJson(item);
+       workspace.add(w);
+      }).toList();
+      print(workspace);
+
+      return workspace;
+
+    } on DioException catch (e) {
+      if (e.response!.statusCode != 200) {
+        print(e.response);
+        return [];
+      }
+    }
+
+    return [];
+  }
+
+  Future<WorkspaceModel> postWorkspace(String workspaceName) async {
+    dio.options.headers['Authorization'] =
+        "Bearer ${UserLogged.first.getUserId}";
+
+    try {
+      final response = await dio.post('https://kanbanboard-nj8m.onrender.com/api/workspace', data: {'name': workspaceName});
+      final newWorkspace = WorkspaceModel.fromJson(response.data);
+      print(response.data);
+      return newWorkspace;
+    } on DioException catch (e) {
+      print(e.response);
+    }
+    
+    throw '';
+
+  }
+}

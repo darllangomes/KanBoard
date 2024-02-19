85c20ce07a39641fbecc14ddd080c82d155d9d6a
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-18 22:02:10 -0300

    :recycle: refactor: refatorar classe WorkspaceModel

--- kandoard/lib/model/workspace_model.dart
+++ kandoard/lib/model/workspace_model.dart
@@ -1,10 +1,22 @@
+import 'dart:convert';
+
 class WorkspaceModel {
-  final String _nameProject;
-  final int _id;
-  String get nameProject => _nameProject;
-  int get projectId => _id;
+  final String workspaceName;
+  final String workspaceId;
+  final String userId;
+  String get getWorkspaceName => workspaceName;
+  String get getWorkspaceId => workspaceId;
+  String get getUserId => userId;
 
-  WorkspaceModel(this._nameProject, this._id);
+  WorkspaceModel({required this.workspaceName, required this.workspaceId, required this.userId});
 
-  
+  factory WorkspaceModel.fromJson(Map<String, dynamic> json){
+    return switch (json) {
+      {'id': String workspaceId,
+      'name': String workspaceName,
+      'userId': String userId}
+      => WorkspaceModel(workspaceName: workspaceName, workspaceId: workspaceId, userId: userId),
+      _ => throw const FormatException('Erro ao carregar projetos.')
+    }; 
+  }
 }

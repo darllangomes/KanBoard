a524e52cce86b3af071a172138dbb72823d00cb1
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-21 15:58:04 -0300

    :lipstick: feat: criar estrutura inicial das colunas no kanban

--- kandoard/lib/view/kanban_board_view.dart
+++ kandoard/lib/view/kanban_board_view.dart
@@ -26,14 +26,54 @@ class _KanbanBoardState extends State<KanbanBoardView> {
           actionsIconTheme: IconThemeData(),
           title: Text(
             boardCard,
-            style: const TextStyle(
-                color: Color(0xFFD9D9D9),
+            style: TextStyle(
+                color: AppColors.white,
                 fontSize: 24,
                 fontWeight: FontWeight.w200,
                 fontStyle: FontStyle.italic),
           ),
           backgroundColor: AppColors.blue,
         ),
-        body: const Placeholder());
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
+            child: Card(
+                color: AppColors.grey,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: AppColors.blue,
+                    ),
+                    borderRadius: BorderRadius.circular(20)),
+                child: SizedBox(
+                    width: 375,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Nome da Coluna',
+                                style: TextStyle(color: AppColors.blue, fontSize: 20, fontWeight: FontWeight.w300),
+                              ),
+                              CircleAvatar(
+                                backgroundColor: AppColors.blue,
+                                child: Text('2', style: TextStyle(color: AppColors.grey),),
+                              )
+                            ],
+                          ),
+
+                          const SizedBox(height: 20,),
+
+                          Card(child: SizedBox(width: 300, child: Text('Tarefa')),)
+                        ],
+
+                        
+                      ),
+                    ))),
+          ),
+        ));
   }
 }

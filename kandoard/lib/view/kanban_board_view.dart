d9ee9f5f3a3558c0ce7a650a18d0a75504bc752d
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-21 16:43:03 -0300

    :lipstick: :feat: transformar componente coluna em um ListView.Builder

--- kandoard/lib/view/kanban_board_view.dart
+++ kandoard/lib/view/kanban_board_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:kandoard/model/board_model.dart';
 import 'package:kandoard/shared/app_colors.dart';
 
 class KanbanBoardView extends StatefulWidget {
@@ -15,65 +14,120 @@ class _KanbanBoardState extends State<KanbanBoardView> {
   Widget build(BuildContext context) {
     final boardCard = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
-        backgroundColor: AppColors.grey,
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: AppColors.white),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.arrow_back)),
-          actionsIconTheme: IconThemeData(),
-          title: Text(
-            boardCard,
-            style: TextStyle(
-                color: AppColors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.w200,
-                fontStyle: FontStyle.italic),
-          ),
-          backgroundColor: AppColors.blue,
+      backgroundColor: AppColors.grey,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: AppColors.white),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+        actionsIconTheme: const IconThemeData(),
+        title: Text(
+          boardCard,
+          style: TextStyle(
+              color: AppColors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w200,
+              fontStyle: FontStyle.italic),
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
-            child: Card(
-                color: AppColors.grey,
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: AppColors.blue,
-                    ),
-                    borderRadius: BorderRadius.circular(20)),
-                child: SizedBox(
-                    width: 375,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: AppColors.blue,
+      ),
+      body: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: ((context, index) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 40, left: 30, right: 30, bottom: 30),
+                child: Card(
+                    color: AppColors.grey,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: AppColors.blue,
+                        ),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: SizedBox(
+                        width: 375,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
                             children: [
-                              Text(
-                                'Nome da Coluna',
-                                style: TextStyle(color: AppColors.blue, fontSize: 20, fontWeight: FontWeight.w300),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Nome da Coluna',
+                                    style: TextStyle(
+                                        color: AppColors.blue,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                  CircleAvatar(
+                                    backgroundColor: AppColors.blue,
+                                    child: Text(
+                                      '2',
+                                      style: TextStyle(color: AppColors.grey),
+                                    ),
+                                  )
+                                ],
                               ),
-                              CircleAvatar(
-                                backgroundColor: AppColors.blue,
-                                child: Text('2', style: TextStyle(color: AppColors.grey),),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Card(
+                                child:
+                                    SizedBox(width: 300, child: Text('Tarefa')),
                               )
                             ],
                           ),
+                        ))),
+              ),
+            );
+          })),
+      // body: Center(
+      //   child: Padding(
+      //     padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
+      //     child: Card(
+      //         color: AppColors.grey,
+      //         elevation: 0,
+      //         shape: RoundedRectangleBorder(
+      //             side: BorderSide(
+      //               color: AppColors.blue,
+      //             ),
+      //             borderRadius: BorderRadius.circular(20)),
+      //         child: SizedBox(
+      //             width: 375,
+      //             child: Padding(
+      //               padding: const EdgeInsets.all(20.0),
+      //               child: Column(
+      //                 children: [
+      //                   Row(
+      //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                     children: [
+      //                       Text(
+      //                         'Nome da Coluna',
+      //                         style: TextStyle(color: AppColors.blue, fontSize: 20, fontWeight: FontWeight.w300),
+      //                       ),
+      //                       CircleAvatar(
+      //                         backgroundColor: AppColors.blue,
+      //                         child: Text('2', style: TextStyle(color: AppColors.grey),),
+      //                       )
+      //                     ],
+      //                   ),
 
-                          const SizedBox(height: 20,),
+      //                   const SizedBox(height: 20,),
 
-                          Card(child: SizedBox(width: 300, child: Text('Tarefa')),)
-                        ],
+      //                   Card(child: SizedBox(width: 300, child: Text('Tarefa')),)
+      //                 ],
 
-                        
-                      ),
-                    ))),
-          ),
-        ));
+      //               ),
+      //             ))),
+      //   ),
+      // )
+    );
   }
 }

762ab48a9e06a681fa847cfe4d048e43b936dcab
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-22 17:34:19 -0300

    :sparkles: feat: criar requisicao getCards para listar os cards criados

---
+++ kandoard/lib/services/cards_service.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:kandoard/model/card_model.dart';
+
+import '../model/user_model.dart';
+
+class CardService {
+  Dio dio = Dio();
+
+  Future<List<CardModel>> getCardsList() async {
+    dio.options.headers['Authorization'] =
+        "Bearer ${UserLogged.first.getUserId}";
+    try {
+      final response =
+          await dio.get('https://kanbanboard-nj8m.onrender.com/api/card');
+      print(response.data);
+      List<CardModel> cards = [];
+      response.data.map((item) {
+        final card = CardModel.fromMap(item);
+        print(card);
+        cards.add(card);
+      }).toList();
+      print(cards);
+      return cards;
+
+    } on DioException catch (e) {
+      print(e.response);
+    }
+    throw 'Não foi possivel adicionar card';
+  }
+}

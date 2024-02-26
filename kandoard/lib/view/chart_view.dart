efebbd3a71f1aae466ee60d3646ce9244af88a14
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-02-26 09:06:06 -0300

    :sparkles: feat: estrutura inicial dos gráficos

--- kandoard/lib/view/chart_view.dart
+++ kandoard/lib/view/chart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kandoard/components/bar_graph.dart';
 import 'package:kandoard/shared/app_colors.dart';
 
 class ChartView extends StatefulWidget {
@@ -6,19 +7,20 @@ class ChartView extends StatefulWidget {
   static const routeName = '/charts';
   @override
   ChartViewState createState() {
-    // TODO: implement createState
     return ChartViewState();
   }
 }
 
 class ChartViewState extends State<ChartView> {
+  List<double> test = [4.0, 10.0, 9.0, 1.0, 2.0, 3.0, 0.0];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.grey,
       appBar: AppBar(
         backgroundColor: AppColors.blue,
-        iconTheme: IconThemeData(color: AppColors.white),
+        iconTheme: IconThemeData(color: AppColors.grey),
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
@@ -28,13 +30,32 @@ class ChartViewState extends State<ChartView> {
         title: Text(
           'Métricas Kanban',
           style: TextStyle(
-              color: AppColors.white,
+              color: AppColors.grey,
               fontSize: 24,
               fontWeight: FontWeight.w200,
               fontStyle: FontStyle.italic),
         ),
       ),
-      body: SingleChildScrollView(child: Column(children: [],)),
+      body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                    children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Throughput',
+                style: TextStyle(fontSize: 24, color: AppColors.blue, fontWeight: FontWeight.w300),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            
+            SizedBox(height: 300, child: BarGraph(weeklySummary: test,),)
+                    ],
+                  ),
+          )),
     );
   }
 }
